727bb789f46ba851d47dfc7a74cf73105e4baa10
gytim <gytim@TIMUR_GUL>
2024-09-13 11:06:15 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.GulahmedovTG.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GulahmedovTG.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tyuiu.GulahmedovTG.Sprint0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GulahmedovTG.Sprint0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
