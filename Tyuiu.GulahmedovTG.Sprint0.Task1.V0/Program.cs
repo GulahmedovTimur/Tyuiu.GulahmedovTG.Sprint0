f64ea728f3739f3a024a56d6d410ee6b1616ec64
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-09-13 12:32:34 +0500

    Task1

--- Tyuiu.GulahmedovTG.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.GulahmedovTG.Sprint0.Task1.V0/Program.cs
@@ -4,10 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            //Вывод на консоль строку Hello, World!
+            Console.WriteLine("Hello, World!");
+
+            //Объявляем переменные
             string name = "Тимур";
             int age = 18;
+
+            //Вывод на консоль строку
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+
+            //Ожидание нажатия клавиши, нужно чтобы окно консоли не закрывалось
             Console.ReadKey();
         }
     }
