7c2c80e77dd3d5b86d2a193cd3524f4a577f5f11
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-09-13 21:39:07 +0500

    Task5

---
+++ Tyuiu.GulahmedovTG.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿namespace Tyuiu.GulahmedovTG.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+
+            else
+            {
+                return (a / b);
+
+            }
+
+        }
+    }
+}
---
+++ Tyuiu.GulahmedovTG.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tyuiu.GulahmedovTG.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.GulahmedovTG.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
