1b4bca14755498885ed787dd8ef6e2fa6e95cbe1
Tyuiu.GulahmedovTG@outlook.com <Tyuiu.GulahmedovTG@outlook.com>
2024-09-13 21:08:49 +0500

    Task4

---
+++ Tyuiu.GulahmedovTG.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿namespace Tyuiu.GulahmedovTG.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
---
+++ Tyuiu.GulahmedovTG.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Tyuiu.GulahmedovTG.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.GulahmedovTG.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
